ac55fb9cc74f40fabcdd52f342ad811d14f1f356
Gunjan Jain <jain10gunjan@gmail.com>
2024-05-28 12:13:31 +0530

    basics - data types in javascript

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //Treat all the javaScript code as a newer version
+// Whereas nowadays without writing this the browser automatically uses use strict functionality
+
+// alert(3 + 3); used for generating a pop up in the browser | however not work in the terminal because we are using nodejs
+
+// console.log(3 + 3)
+// console.log("Gunjan")
+
+let name = "Gunjan"
+let age = 18
+let isLoggedIn = true
+
+// Number DataType -  2 to the power 53 is the range of the number
+// bigInt dataType - When the number is very big example using in the stock market websites or when we have the large data
+// string dataType - ""
+// boolean - yes or no
+// null - standalone value it is in the javascript [Representation of an empty value]
+// undefined - abhi value assign nahi hui hai
+// symbol - when we talk about some uniqueness
+// object - 
+
+// typeof null is object
+// typeof undefined is undefined
+
+console.log(typeof null);
+console.log(typeof undefined);
+
